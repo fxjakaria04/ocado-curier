1b8945334cd6a2fbac4a6ce239805258f3037b8d
fxjakaria04 <freelancerx2023@gmail.com>
2024-10-19 17:01:53 +0600

    Add parcel page added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['index.html', 'fresh.html'],
+  content: ['index.html', 'add-parcel.html'],
   theme: {
     extend: {},
   },
