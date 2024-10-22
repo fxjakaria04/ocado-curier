2f6825f057fca136ee3cbc7ccbef62b6bc469f3e
fxjakaria04 <freelancerx2023@gmail.com>
2024-10-22 18:15:01 +0600

    few task left

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['index.html', 'add-parcel.html', 'ParcelRequestList.html'],
+  content: ['index.html', 'add-parcel.html', 'ParcelRequestList.html', 'allParcel.html', 'invoice.html'],
   theme: {
     extend: {},
   },
