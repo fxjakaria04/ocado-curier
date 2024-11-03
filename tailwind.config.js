8e0b57153ba2964a9c501566e7decea8e4c1ce55
fxjakaria04 <freelancerx2023@gmail.com>
2024-11-03 17:08:33 +0600

    notification added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['index.html', 'add-parcel.html', 'ParcelRequestList.html', 'allParcel.html', 'invoice.html', 'login.html', 'status.html', 'profile.html', 'editProfile.html', 'changePassword.html', 'paymentUpdate.html'],
+  content: ['index.html', 'add-parcel.html', 'ParcelRequestList.html', 'allParcel.html', 'invoice.html', 'login.html', 'status.html', 'profile.html', 'editProfile.html', 'changePassword.html', 'paymentUpdate.html', 'paymentChildView.html'],
   theme: {
     extend: {},
   },
