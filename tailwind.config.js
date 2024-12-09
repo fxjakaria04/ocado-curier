464d632597faa0d98211fdf960cde0b450c904f0
fxjakaria04 <freelancerx2023@gmail.com>
2024-12-09 13:44:52 +0600

    push everything

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['index.html', 'add-parcel.html', 'ParcelRequestList.html', 'allParcel.html', 'invoice.html', 'login.html', 'status.html', 'profile.html', 'editProfile.html', 'changePassword.html', 'paymentUpdate.html', 'paymentChildView.html', 'paymentInvoice.html', 'component.html', 'component2.html'],
+  content: [ './*.html'],
   theme: {
     extend: {},
   },
